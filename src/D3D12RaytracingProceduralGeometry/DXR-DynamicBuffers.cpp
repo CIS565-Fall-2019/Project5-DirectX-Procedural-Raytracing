12cb44840ee43f0916324c7aa0d546f0a668b461
Taylor Nelms <taylornelms15@gmail.com>
2019-10-15 08:32:52 -0400

    First attempt at not fucking up the code

--- src/D3D12RaytracingProceduralGeometry/DXR-DynamicBuffers.cpp
+++ src/D3D12RaytracingProceduralGeometry/DXR-DynamicBuffers.cpp
@@ -111,7 +111,10 @@ void DXProceduralProject::CreateConstantBuffers()
 //		structured buffers are for structs that have dynamic data (e.g lights in a scene, or AABBs in this case)
 void DXProceduralProject::CreateAABBPrimitiveAttributesBuffers()
 {
+	auto device = m_deviceResources->GetD3DDevice();
+	auto num_objects = m_aabbs.size();
 
+	m_aabbPrimitiveAttributeBuffer.Create(device, num_objects, 1, L"AABB Primitive Attribute Buffer");
 }
 
 // LOOKAT-2.1: Update camera matrices stored in m_sceneCB.
@@ -164,6 +167,11 @@ void DXProceduralProject::UpdateAABBPrimitiveAttributes(float animationTime)
 		// You can infer what the bottom level AS space to local space transform should be.
 		// The intersection shader tests in this project work with local space, but the geometries are provided in bottom level 
 		// AS space. So this data will be used to convert back and forth from these spaces.
+		auto transformMatrix = XMMatrixMultiply(mScale, mRotation);
+		transformMatrix = XMMatrixMultiply(transformMatrix, mTranslation);
+		auto invTransformMatrix = XMMatrixInverse(nullptr, transformMatrix);
+		m_aabbPrimitiveAttributeBuffer[primitiveIndex].localSpaceToBottomLevelAS = transformMatrix;
+		m_aabbPrimitiveAttributeBuffer[primitiveIndex].bottomLevelASToLocalSpace = invTransformMatrix;
 	};
 
 	UINT offset = 0;
